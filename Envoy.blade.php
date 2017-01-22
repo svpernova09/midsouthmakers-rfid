6e1c96a0f804b95de250a8504bb4ce63fec86942
Joe Ferguson <joe@joeferguson.me>
2017-01-22 15:59:32 -0600

    ✨ 💄 ⬆️ 🚀 ❄️ Add Envoy for deployment, Add Artisan Command to Create Admins

---
+++ Envoy.blade.php
@@ -0,0 +1,10 @@
+@servers(['prod' => 'mmrfid@midsouthmakers.org'])
+
+@task('deploy:prod', ['on' => 'prod'])
+cd /home/mmrfid/midsouthmakers-rfid
+php artisan down
+git pull origin master
+composer install --no-dev
+php artisan migrate --force
+php artisan up
+@endtask
