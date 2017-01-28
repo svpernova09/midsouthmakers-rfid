5d4c0c082746f19300bc3fabf2ba61636e17673b
Joe Ferguson <joe@joeferguson.me>
2017-01-27 21:22:59 -0600

    ✨ Holy crap the API works!

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
                 <div class="panel-heading">Dashboard</div>
 
                 <div class="panel-body">
-                    You are logged in!
+                    <passport-personal-access-tokens></passport-personal-access-tokens>
                 </div>
             </div>
         </div>
