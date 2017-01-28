112f4f663fbe6c7adb6bea0d1635c10ca3794129
Joe Ferguson <joe@joeferguson.me>
2017-01-27 19:16:44 -0600

    🚑 Travis doesn't like strings? wtf

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,9 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
+            $table->text('name');
+            $table->text('email')->unique();
+            $table->text('password');
             $table->boolean('admin')->default(false);
             $table->rememberToken();
             $table->timestamps();
