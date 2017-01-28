689dd19ca6192bc387fd57eba6106871b9d10473
Joe Ferguson <joe@joeferguson.me>
2017-01-27 19:18:28 -0600

    🚑 Travis is odd

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,9 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->text('name');
-            $table->text('email')->unique();
-            $table->text('password');
+            $table->string('name');
+            $table->string('email', 255)->unique();
+            $table->string('password');
             $table->boolean('admin')->default(false);
             $table->rememberToken();
             $table->timestamps();
