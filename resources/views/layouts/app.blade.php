d70a509333b3e71f69ccd2f0722531606e17f2d5
Joe Ferguson <joe@joeferguson.me>
2017-02-23 21:14:38 -0600

    ✨ ❄️ Add waiver link to the layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -50,6 +50,7 @@
                 <ul class="nav navbar-nav navbar-right">
                     <!-- Authentication Links -->
                     @if (Auth::guest())
+                        <li><a href="{{ url('/sign-waiver') }}">Sign Waiver</a></li>
                         <li><a href="{{ url('/login') }}">Login</a></li>
                         <li><a href="{{ url('/register') }}">Register</a></li>
                     @else
