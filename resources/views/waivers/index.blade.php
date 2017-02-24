6afe45efd2f173fd2a095a69b7977ce1c834f09b
Joe Ferguson <joe@joeferguson.me>
2017-02-23 21:19:53 -0600

    ✨ Tweak waiver page formatting

--- resources/views/waivers/index.blade.php
+++ resources/views/waivers/index.blade.php
@@ -3,19 +3,28 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-md-10 col-md-offset-1">
+            <div class="col-md-4 col-md-offset-4">
                 <div class="panel panel-default">
                     <div class="panel-heading">Sign Waiver</div>
                     <div class="panel-body">
                         <p>
+                            All visitors to the MidsouthMakers location are required to sign a short waiver.
+                        </p>
+                        <p class="pull-left">
                             <a href="/sign-waiver/individual">
                                 <button class="btn btn-primary">Individual Waiver</button>
                             </a>
                         </p>
-                        <p>
+                        <p class="pull-right">
                             <a href="/sign-waiver/dependent">
                                 <button class="btn btn-primary">Dependent Waiver</button>
                             </a>
+                        </p>
+                    </div>
+                    <div class="panel-body">
+                        <p>
+                            <h4>Privacy Policy</h4>
+                            MidsouthMakers.org will never share your information with any 3rd parties.
                         </p>
                     </div>
                 </div>
