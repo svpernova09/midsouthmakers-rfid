1afd89f3703b719e4b3b49d255a043be799c7a8f
Joe Ferguson <joe@joeferguson.me>
2020-12-06 13:33:16 -0600

    it never works right the first time

--- app/Http/Controllers/Auth/DiscordAuthController.php
+++ app/Http/Controllers/Auth/DiscordAuthController.php
@@ -5,11 +5,11 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\SendDiscordAuthEmailRequest;
 use App\Mail\VerifyDiscordAccount;
-use App\Mail\WaiverSigned;
 use App\User;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Password;
 use Illuminate\Support\Facades\Request;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
@@ -22,7 +22,7 @@ class DiscordAuthController extends Controller
         $user = User::where('email', $email)->first();
         $user->author_id = $request->input('author_id');
         $user->discord_username = $request->input('discord_username');
-        $user->discord_hash = Hash::make($user->author_id.$user->discord_username.$email);
+        $user->discord_hash = Password::getRepository()->createNewToken();
 
         Cache::add('discord_auth_'.$user->id, $user);
         Mail::to($user->email)->send(new VerifyDiscordAccount($user));
@@ -35,12 +35,16 @@ class DiscordAuthController extends Controller
         $input_hash = $request->input('hash');
         $user = Auth::user();
         $cached_user = Cache::get('discord_auth_'.$user->id);
+
         if ($input_hash === $cached_user->discord_hash)
         {
             $user->discord_id = $cached_user->author_id;
             $user->discord_username = $cached_user->discord_username;
+            $user->save();
         } else {
             throw new BadRequestHttpException('Bad Request');
         }
+
+        return response()->redirectToRoute('home');
     }
 }
