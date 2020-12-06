8834596adc4933fd1951f152e18f7649cb510185
Joe Ferguson <joe@joeferguson.me>
2020-12-06 01:55:36 -0600

    Add webhook server

---
+++ app/Http/Controllers/Auth/DiscordAuthController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\SendDiscordAuthEmailRequest;
+use Illuminate\Support\Facades\Request;
+
+
+class DiscordAuthController extends Controller
+{
+    public function sendAuthEmail(SendDiscordAuthEmailRequest $request){
+
+    }
+}
