5a175e014532651b5ce3b2b8ba1077782608a5b9
Joe Ferguson <joe@joeferguson.me>
2020-12-11 12:39:00 -0600

    re-enable schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -31,7 +31,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-//        $schedule->command(SyncDiscordRolesWithMembers::class)->everyFiveMinutes();
+        $schedule->command(SyncDiscordRolesWithMembers::class)->everyFiveMinutes();
         $schedule->command(CacheDiscordInfo::class)->everyThreeHours();
     }
 
